7612fb827b43c337e76849ee11d070d218546684
numahata1011 <ynumahata@gmail.com>
2021-09-10 15:55:51 +0000

    2021/09/10_2

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -36,7 +36,7 @@
                     <div class="form-group row">
                         <label class="col-md-2">趣味</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="5">{{ old('introduction') }}</textarea>
+                            <textarea class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
                         </div>
                     </div>
 
